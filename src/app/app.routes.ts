01442dba0f8641565f91212b661646009540311a
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2024-10-21 23:56:58 +0700

    fix: ha fix code router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,19 @@
 import { Routes } from '@angular/router';
-import { DanhSachGiaoDich } from './danhsachgiaodich';
 import { ChitietComponent } from './chitiet/chitiet.component';
 import { DanhsachgiaodichComponent } from './danhsachgiaodich/danhsachgiaodich.component';
 
 const routerConfig: Routes = [
     {
-        path : '',
-        component : DanhsachgiaodichComponent,
-        title : 'Danh sách'
+        path: '',
+        component: DanhsachgiaodichComponent,
+        title: 'Danh sách'
     },
     {
-        path : 'chitiet\id',
-        component : ChitietComponent,
-        title : 'Chi tiết giao dịch'
+        path: 'chitiet\id',
+        component: ChitietComponent,
+        title: 'Chi tiết giao dịch'
     }
 
 ];
 
 export default routerConfig;
-
